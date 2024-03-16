a53af5cf769536a9e42ef0dfbab7d0ee3431351f
Prachi Patel <prachikpatel@gmail.com>
2024-03-16 17:43:50 -0400

    #56 Created LogWriterView Class in View package

---
+++ src/main/java/Views/LogWriterView.java
@@ -0,0 +1,13 @@
+package Views;
+
+import Models.Observable;
+import Models.Observer;
+
+public class LogWriterView implements Observer {
+
+
+    @Override
+    public void update(Observable o) {
+
+    }
+}
