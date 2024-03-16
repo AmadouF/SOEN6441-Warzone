8e0d9ccf1369aa86680ac26aa66ad885a9901ffe
Prachi Patel <prachikpatel@gmail.com>
2024-03-16 18:01:47 -0400

    #56 Updating LogWriterView Class and Constants Class

--- src/main/java/Constants/Constants.java
+++ src/main/java/Constants/Constants.java
@@ -9,4 +9,5 @@ public class Constants {
     public static final String CONTINENTS = "[continents]";
     public static final String COUNTRIES = "[countries]";
     public static final String BORDERS = "[borders]";
+    public static final String GAMELOGS_FILE_NAME = "GameLogs.txt";
 }
--- src/main/java/Views/LogWriterView.java
+++ src/main/java/Views/LogWriterView.java
@@ -1,13 +1,32 @@
 package Views;
 
+import Models.LogEntryBuffer;
 import Models.Observable;
 import Models.Observer;
+import Constants.Constants;
+
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 public class LogWriterView implements Observer {
 
-
+    LogEntryBuffer d_logEntryBuffer;
     @Override
-    public void update(Observable o) {
+    public void update(Observable l_observable) {
+        d_logEntryBuffer = (LogEntryBuffer) l_observable;
+        File l_logfile = new File(Constants.GAMELOGS_FILE_NAME);
+        String l_logMessage = d_logEntryBuffer.getD_logMessage();
 
+        try{
+            if(l_logMessage.equals("Initializing the Game ......"+System.lineSeparator())) {
+                Files.newBufferedWriter(Paths.get(Constants.GAMELOGS_FILE_NAME), StandardOpenOption.TRUNCATE_EXISTING).write(" ");
+            }
+            Files.write(Paths.get(Constants.GAMELOGS_FILE_NAME), l_logMessage.getBytes(StandardCharsets.US_ASCII), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
+        }catch(Exception l_e){
+            l_e.printStackTrace();
+        }
     }
 }
