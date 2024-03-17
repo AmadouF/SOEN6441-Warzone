0006823b685ae26559b3b46815952742c19cf60e
Jay Patel <jayashokpatel29@gmail.com>
2024-03-17 17:16:38 -0400

    Fix - Fixed small javaDoc error

--- src/main/java/Models/Card.java
+++ src/main/java/Models/Card.java
@@ -7,7 +7,7 @@ public interface Card extends Order {
     /**
      * Validate the card before creating order
      *
-     * @param p_gameState
+     * @param p_gameState current state of the game
      * @return true of false
      */
     public Boolean checkValidOrder(GameState p_gameState);
