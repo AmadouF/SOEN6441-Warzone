331e152bf2caf0d12bedeb771c0c84184631e0e5
Amadou Fall <papaamadofall@gmail.com>
2024-03-17 22:28:07 -0400

    Add javadoc

--- src/main/java/Constants/Constants.java
+++ src/main/java/Constants/Constants.java
@@ -1,14 +1,47 @@
 package Constants;
+
 /**
- * This class holds the constant data members.
+ * This class holds constant data members used throughout the application.
  */
 public class Constants {
+
+    /**
+     * Represents the key for accessing arguments in maps.
+     */
     public static final String ARGUMENT = "argument";
+
+    /**
+     * Represents the key for accessing operations in maps.
+     */
     public static final String OPERATION = "operation";
+
+    /**
+     * Represents the default width of the console.
+     */
     public static final int CONSOLE_WIDTH = 80;
+
+    /**
+     * Represents the string indicating the start of continents data.
+     */
     public static final String CONTINENTS = "[continents]";
+
+    /**
+     * Represents the string indicating the start of countries data.
+     */
     public static final String COUNTRIES = "[countries]";
+
+    /**
+     * Represents the string indicating the start of borders data.
+     */
     public static final String BORDERS = "[borders]";
+
+    /**
+     * Represents the filename for the game logs.
+     */
     public static final String GAMELOGS_FILE_NAME = "GameLogs.txt";
-    public static final String STARTING_THE_GAME_LOG_MESSAGE = "----------- Starting the Game -----------" +System.lineSeparator();
+
+    /**
+     * Represents the message logged when the game starts.
+     */
+    public static final String STARTING_THE_GAME_LOG_MESSAGE = "----------- Starting the Game -----------" + System.lineSeparator();
 }
